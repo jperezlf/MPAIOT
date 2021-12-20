f667315e3dec1a1e31ca07cfd983d3021df1ef5c
JoseLuis <jperezlf@alumnos.unex.es>
2021-12-20 11:16:27 +0100

    Añadido opciones de configuracion

--- training/train.php
+++ training/train.php
@@ -6,6 +6,8 @@ class execute_training
 
     public function run()
     {
+
+        $array_data = [];
         $command = "cd .. && cd algorithms/$this->type/model/ && python3 charge_model.py " . $array_data;
         $result = shell_exec($command);
         $command = "cd .. && cd algorithms/$this->type/model/ && python3 charge_model.py " . $array_data;
