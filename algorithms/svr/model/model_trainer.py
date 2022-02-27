278b972fa10f66e927ee6d42ceb86dbe1a5da812
JoseLuis <jperezlf@alumnos.unex.es>
2022-02-27 18:31:21 +0100

    Algoritmo svr mejorado

--- algorithms/svr/model/model_trainer.py
+++ algorithms/svr/model/model_trainer.py
@@ -10,7 +10,6 @@ pandas.set_option('display.max_rows', 500)
 pandas.set_option('display.max_columns', 500)
 pandas.set_option('display.width', 1000)
 
-
 path_to_project = sys.argv[1]
 
 
@@ -23,26 +22,24 @@ def mean_absolute_error(y_true, y_pred):
 # load data
 if sys.argv[1]:
     path_to_project = sys.argv[1]
-    data = pandas.read_csv(path_to_project + '/training/training.csv', header=0)
+    data = pandas.read_csv(path_to_project + '/crones_datos/file.csv', header=0)
 else:
     data = pandas.read_csv('training.csv', header=0)
 dataset = data.drop_duplicates()
 feature_names = dataset.columns.values
 
-features_to_delete = np.array(['value'])
+features_to_delete = np.array(['Temperature'])
 feature_names = np.setdiff1d(feature_names, features_to_delete)
 
-Y_train = dataset.filter(items=['value'])
+Y_train = dataset.filter(items=['Temperature'])
 X_train = dataset.filter(items=feature_names)
 
 # Split dataset into training set and test set
-X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, shuffle=True, test_size=0.2, random_state=123)
-
-clf = SVR()
-clf.fit(X_train, Y_train.values.ravel())
+svm_reg = SVR(kernel='rbf', C=1000000, epsilon=0.001)
+svm_reg.fit(X_train, Y_train)
 
 if sys.argv[1]:
     name = path_to_project + '/algorithms/svr/model/train.dat'
 else:
     name = 'train.dat'
-pickle.dump(clf, open(name, "wb"))
+pickle.dump(svm_reg, open(name, "wb"))
